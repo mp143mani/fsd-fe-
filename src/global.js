37d3bf9f53ca7483876c022f607c2c3f2ec6b26f
mp143mani <mani143tech@gmail.com>
2024-08-03 21:26:28 +0530

    s

--- src/global.js
+++ src/global.js
@@ -1,7 +1,6 @@
-// export var API = "https://b46-wd-backend.onrender.com";
+// export var API = "https://fsd-be-nqtm.onrender.com";
 
 
-// export var API ="https://66a5ee3523b29e17a1a14baf.mockapi.io/"
 
 export var API = "https://66a5ee3523b29e17a1a14baf.mockapi.io/";
 
